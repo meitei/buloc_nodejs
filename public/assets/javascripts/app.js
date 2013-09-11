185d840ef860e59597d156df34dc310bb4153323
ooishi <haruo.ooishi@gmail.com>
2013-09-11 23:01:13 +0900

    イベントの仕組みを変更中

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -5,18 +5,34 @@ define([
   // app main
   var app = {};
 
+  app.event = {};
+  _.extend(app.event, Backbone.Events);
+
+  app.viewParts = [
+    {id: 'attribute', view: 'AttributeView', collection: 'Attributes'}
+  ];
+
+  // app.views = {};
+  // _.each(app.viewParts, function(vp){
+  app.createView = function(id) {
+    var vp = _.find(app.viewParts, function(vp){ return vp.id == id; });
+    if(!vp) return (void 0);
+    var C = require('collections/' + vp.collection);
+    var V = require('views/pc/' + vp.view);
+    return new V({collection: new C()});
+  };
+
   // any process
   app.init = function() {
-
-  }
-
-  // app views
-  app.views = {};
-
-  var Attributes = require('collections/Attributes');
-  var AttributeView = require('views/pc/AttributeView');
-  app.collections.attributes = new Attributes();
-  app.views.attribute = new AttributeView({collection: app.collections.attributes});
+    var self = this;
+    // set event listener.
+    _.each(app.viewParts, function(vp){
+      this.event.on('open:' + vp.id, function(obj, opt) {
+        var view = self.createView(vp.id);
+        view.open(opt);
+      });
+    });
+  };
 
   return app;
 });
